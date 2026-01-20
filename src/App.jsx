1d66b51c48438d077d2a87396bc96c0e902c2f8e
abishekbabu1905 <abishekmaha1905@gmail.com>
2026-01-20 22:46:03 +0530

    3dportfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,24 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { useRef } from "react";
-
-const RotatingCube = () => {
-    const meshRef = useRef();
-
-    useFrame(() => {
-        if (meshRef.current) {
-            meshRef.current.rotation.x += 0.01;
-            meshRef.current.rotation.y += 0.01;
-            meshRef.current.rotation.z += 0.01;
-        }
-    });
-
-    return (
-        <mesh ref={meshRef}>
-            <cylinderGeometry args={[1, 1, 2, 32]} />
-            <meshStandardMaterial color="#468585" emissive="#468585" emissiveIntensity={0.3} />
-        </mesh>
-    );
-};
+import React from 'react';
 
 const App = () => {
     return (
-        <Canvas style={{ height: "100vh", width: "100vw" }}>
-            <OrbitControls enableZoom enablePan enableRotate />
-            <directionalLight position={[1, 1, 1]} intensity={2} color={0x9cdba6} />
-            <ambientLight intensity={0.5} />
-            <color attach="background" args={["#f0f0f0"]} />
-            <RotatingCube />
-        </Canvas>
+        <header className="fixed top-0 right-0 left-0 z-50 bg-black/90">
+            <div className="max-w-7xl mx-auto">
+                <div className="flex justify-between items-center py-5 mx-auto c-space">
+                    <a
+                        href="/"
+                        className="text-neutral-400 font-bold text-xl hover:text-white transition-colors"
+                    >
+                        Abishekbabu
+                    </a>
+
+                    <button className="invert">
+                        <img src={pub} alt="menu" className="w-6 h-6" />
+                    </button>
+                </div>
+            </div>
+        </header>
     );
-};
+}
 
 export default App;
