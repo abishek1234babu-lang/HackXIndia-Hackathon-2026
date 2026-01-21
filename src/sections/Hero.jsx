b5b125d4aacc93ab6c26765e1df0f7f23d708ddd
abishekbabu1905 <abishekmaha1905@gmail.com>
2026-01-21 09:22:45 +0530

    3dportfolio

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,52 +1,65 @@
-import React, { Suspense } from 'react'
-import { Canvas } from "@react-three/fiber";
-import { PerspectiveCamera } from "@react-three/drei";
-import CanvasLoader from "../components/CanvasLoader.jsx";
+import { Leva } from 'leva';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useMediaQuery } from 'react-responsive';
+import { PerspectiveCamera } from '@react-three/drei';
+
+import Cube from '../components/Cube.jsx';
+import Rings from '../components/Rings.jsx';
+import ReactLogo from '../components/ReactLogo.jsx';
+import Button from '../components/Button.jsx';
+import Target from '../components/Target.jsx';
+import CanvasLoader from '../components/Loading.jsx';
+import HeroCamera from '../components/HeroCamera.jsx';
+import { calculateSizes } from '../constants/index.js';
 import HackerRoom from "../components/HackerRoom.jsx";
-import { Leva, useControls } from "leva";
-import {useMediaQuery} from "react-responsive";
 
 const Hero = () => {
-    const x = useControls('HackerRoom', {
-        rotationX: { value: 2.5, min: -10, max: 10 },
-        rotationY: { value: 2.5, min: -10, max: 10 },
-        rotationZ: { value: 2.5, min: -10, max: 10 },
-        positionX: { value: 0, min: -10, max: 10 },
-        positionY: { value: 0, min: -10, max: 10 },
-        positionZ: { value: 0, min: -10, max: 10 },
-        scale: { value: 1, min: 0.1, max: 5 },
-    });
-    const isMobile=useMediaQuery({maxWidth:768})
+    // Use media queries to determine screen size
+    const isSmall = useMediaQuery({ maxWidth: 440 });
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
     return (
-        <section className='min-h-screen w-full flex flex-col relative'>
-            <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
-                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
-                    Hi I am Abishek <span className="waving-hand">👋</span>
+        <section className="min-h-screen w-full flex flex-col relative" id="home">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+                    Hi, I am Adrian <span className="waving-hand">👋</span>
                 </p>
-                <p className="hero_tag text-gray_gradient">Emerging Software developer</p>
+                <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
             </div>
 
-            <Leva/>
-
-            <div className='w-full h-full absolute inset-0'>
+            <div className="w-full h-full absolute inset-0">
                 <Canvas className="w-full h-full">
                     <Suspense fallback={<CanvasLoader />}>
+                        {/* To hide controller */}
+                        <Leva hidden />
                         <PerspectiveCamera makeDefault position={[0, 0, 30]} />
 
-                        <HackerRoom
-                            position={[-0.2, -9.0, 0.0]}
-                            rotation={[0, -Math.PI, 0.0]}
-                            scale={isMobile ? 0.07 :0.1}
-                        />
+                        <HeroCamera isMobile={isMobile}>
+                            <HackerRoom scale={sizes.deskScale} position={sizes.deskPosition} rotation={[0.1, -Math.PI, 0]} />
+                        </HeroCamera>
+
+                        <group>
+                            <Target position={sizes.targetPosition} />
+                            <Cube position={sizes.cubePosition} />
+                        </group>
 
                         <ambientLight intensity={1} />
                         <directionalLight position={[10, 10, 10]} intensity={0.5} />
                     </Suspense>
                 </Canvas>
             </div>
+
+            <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+                <a href="#about" className="w-fit">
+                    <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+                </a>
+            </div>
         </section>
-    )
-}
+    );
+};
 
 export default Hero;
